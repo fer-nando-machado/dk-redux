3530edb647ea230c4d92f915c633bbb2b7e7ad8a
Fernando Machado <lfmachado90@gmail.com>
2024-07-21 11:56:04 +0200

    introduces flickering

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Game from "./Game";
 function App() {
   return (
     <div className="App">
-      <h1>DK</h1>
+      <b>DK</b>
       <Game />
       <a href="https://github.com/fer-nando-machado/react-dk">
         <img src={GitHub} alt="GitHub" height={25} />
