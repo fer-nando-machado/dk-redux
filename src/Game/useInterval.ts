b8ed2ec506b32a6cda87d7efdb00515ad64ce312
Fernando Machado <lfmachado90@gmail.com>
2024-07-22 21:11:18 +0200

    introduces callbackRef on useInterval to allow refreshes

--- src/Game/useInterval.ts
+++ src/Game/useInterval.ts
@@ -8,9 +8,18 @@ const useInterval = (callback: () => void, ms: number = REFRESH_RATE) => {
   const paused = useSelector((state: RootState) => state.options.paused);
   const interval = useRef<NodeJS.Timeout | null>(null);
 
+  const callbackRef = useRef<() => void>();
+  const tick = () => {
+    if (!callbackRef.current) return;
+    callbackRef.current();
+  };
+  useEffect(() => {
+    callbackRef.current = callback;
+  }, [callback]);
+
   useEffect(() => {
     if (!paused) {
-      interval.current = setInterval(callback, ms);
+      interval.current = setInterval(tick, ms);
     } else if (interval.current) {
       clearInterval(interval.current);
       interval.current = null;
