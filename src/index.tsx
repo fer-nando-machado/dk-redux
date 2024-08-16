0de2e0dd81aaded723217bba1662b198f6f2ac60
Fernando Machado <lfmachado90@gmail.com>
2024-08-16 11:47:39 +0200

    renders App at window.onLoad

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.scss";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+window.onload = () => {
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+};
